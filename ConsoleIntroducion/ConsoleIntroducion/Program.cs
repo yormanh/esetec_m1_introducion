0f1c6c9510d1fdf1fa7f1ffa2e0389c8851c7cde
Yorman <yormanh@gmail.com>
2023-05-25 20:58:39 +0100

    Introducion a al c#

---
+++ ConsoleIntroducion/ConsoleIntroducion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleIntroducion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Presiona una tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
