9df607f87a28029a315d0d5ce80579a3e72dc2b4
Yorman <yormanh@gmail.com>
2023-05-26 19:25:46 +0100

    Pedida entrada por teclado y mostrando

---
+++ Introduccion2/Introduccion2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            string entradaUsuario;
+            string entradaNombre;
+
+            Console.Write("Escribe tu edad: ");
+            entradaUsuario = Console.ReadLine();
+
+            Console.Write("Escribe tu nombre: ");
+            entradaNombre = Console.ReadLine();
+
+            Console.WriteLine("Tu edad es: " + entradaUsuario);
+            Console.WriteLine("Tu nombre es: " + entradaNombre);
+
+            Console.WriteLine("Presiona Enter para salir");
+            Console.ReadLine();
+        }
+    }
+}
