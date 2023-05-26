44786623a9cce31ea98ec297b218bb837db3e0f3
Yorman <yormanh@gmail.com>
2023-05-26 20:06:17 +0100

    Suma y resta de numeros

--- Introduccion2/Introduccion2/Program.cs
+++ Introduccion2/Introduccion2/Program.cs
@@ -10,18 +10,34 @@ namespace Introduccion2
     {
         static void Main(string[] args)
         {
-            int edad;
-            string entradaUsuario;
-            string entradaNombre;
+            string valor1, valor2; 
+            int resultado;
+            int valor1Entero, valor2Entero;
 
-            Console.Write("Escribe tu edad: ");
-            entradaUsuario = Console.ReadLine();
+            Console.Write("Entra el primer numero: ");
+            valor1 = Console.ReadLine();
 
-            Console.Write("Escribe tu nombre: ");
-            entradaNombre = Console.ReadLine();
+            Console.Write("Entra el segundo numero: ");
+            valor2 = Console.ReadLine();
 
-            Console.WriteLine("Tu edad es: " + entradaUsuario);
-            Console.WriteLine("Tu nombre es: " + entradaNombre);
+            //resultado = Convert.ToInt32(valor1) + Convert.ToInt32(valor2);
+
+            valor1Entero = Convert.ToInt32(valor1);
+            valor2Entero = Convert.ToInt32(valor2);
+
+            resultado = valor1Entero + valor2Entero;
+            Console.WriteLine(valor1 + " + " + valor2 + " = " + resultado);
+
+            resultado = valor1Entero - valor2Entero;
+            Console.WriteLine(valor1 + " - " + valor2 + " = " + resultado);
+
+            resultado = valor1Entero * valor2Entero;
+            Console.WriteLine(valor1 + " * " + valor2 + " = " + resultado);
+
+            resultado = valor1Entero / valor2Entero;
+            Console.WriteLine(valor1 + " / " + valor2 + " = " + resultado);
+
+
 
             Console.WriteLine("Presiona Enter para salir");
             Console.ReadLine();
