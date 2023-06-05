0365a0fcbd43cfc15a6d1961c93e6913a4c12071
Yorman <yormanh@gmail.com>
2023-06-05 20:55:28 +0100

    Primer ejemplo en Unity

---
+++ Unity/IntroducionUnity/Assets/MinecraftBoxes/Components/MBillboard.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace MinecraftTool
+{
+	[ExecuteInEditMode()]
+	public class MBillboard : MonoBehaviour 
+	{	
+		public bool bPauseBillboard;
+
+		void OnRenderObject()
+		{
+			if(bPauseBillboard) return;
+			
+			Camera sceneCamera = !EditorApplication.isPlaying
+			? SceneView.lastActiveSceneView.camera
+			: Camera.main;
+
+			if(sceneCamera)
+			{
+				var dir = sceneCamera.transform.position - transform.position;
+				dir.y = 0;
+				dir.Normalize();
+
+				transform.rotation = Quaternion.LookRotation(-dir);
+			}
+		}
+	}
+
+}
---
+++ Unity/IntroducionUnity/Assets/MinecraftBoxes/Editor/MBillboardEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using MinecraftTool;
+using UnityEditor;
+
+[CustomEditor(typeof(MBillboard))]
+public class MBillboardEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        MBillboard t = target as MBillboard;
+        if(!t) return;
+
+        var origin = t.transform.position;
+        var destiny = t.transform.position + Vector3.up*2;
+
+        Handles.DrawLine(origin, destiny);
+        Handles.Label(destiny + Vector3.up * .5f
+            , t.transform.name + "\n"
+            + "Active: " + (!t.bPauseBillboard).ToString()
+        );
+    }
+
+
+}
